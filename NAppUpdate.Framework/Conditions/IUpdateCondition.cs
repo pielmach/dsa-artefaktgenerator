4c19a533a43a2020d9f8c93b396e371dfabb642b
Michael Prim <pielmach@outlook.de>
2022-04-18 13:44:47 +0200

    Remove entire NAppUpdate dependency and cleanup solution

--- NAppUpdate.Framework/Conditions/IUpdateCondition.cs
+++
@@ -1,9 +0,0 @@
-﻿using NAppUpdate.Framework.Common;
-
-namespace NAppUpdate.Framework.Conditions
-{
-    public interface IUpdateCondition : INauFieldsHolder
-    {
-        bool IsMet(Tasks.IUpdateTask task);
-    }
-}
