55a393358c5930365e6beea4f9c13fba8242b0aa
Michael Prim <pielmach@outlook.de>
2022-04-24 19:13:15 +0200

    Adding licence and about forms, icon and finalizing installer

--- ArtefaktGenerator/AboutForm.Designer.cs
+++ ArtefaktGenerator/AboutForm.Designer.cs
@@ -28,12 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.labelAbout = new System.Windows.Forms.Label();
+            this.linkLabelGitHub = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // labelAbout
+            // 
+            this.labelAbout.AutoSize = true;
+            this.labelAbout.Location = new System.Drawing.Point(40, 37);
+            this.labelAbout.MaximumSize = new System.Drawing.Size(730, 0);
+            this.labelAbout.Name = "labelAbout";
+            this.labelAbout.Size = new System.Drawing.Size(716, 250);
+            this.labelAbout.TabIndex = 0;
+            this.labelAbout.Text = resources.GetString("labelAbout.Text");
+            // 
+            // linkLabelGitHub
+            // 
+            this.linkLabelGitHub.AutoSize = true;
+            this.linkLabelGitHub.LinkArea = new System.Windows.Forms.LinkArea(40, 89);
+            this.linkLabelGitHub.Location = new System.Drawing.Point(40, 309);
+            this.linkLabelGitHub.MaximumSize = new System.Drawing.Size(780, 30);
+            this.linkLabelGitHub.Name = "linkLabelGitHub";
+            this.linkLabelGitHub.Size = new System.Drawing.Size(757, 30);
+            this.linkLabelGitHub.TabIndex = 1;
+            this.linkLabelGitHub.TabStop = true;
+            this.linkLabelGitHub.Text = "Alle weiteren Informationen auf GitHub: https://github.com/pielmach/dsa-artefaktg" +
+    "enerator";
+            this.linkLabelGitHub.UseCompatibleTextRendering = true;
+            this.linkLabelGitHub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelGitHub_LinkClicked);
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 387);
+            this.Controls.Add(this.linkLabelGitHub);
+            this.Controls.Add(this.labelAbout);
+            this.Name = "AboutForm";
             this.Text = "AboutForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelAbout;
+        private System.Windows.Forms.LinkLabel linkLabelGitHub;
     }
 }
---
+++ ArtefaktGenerator/Resources/readme.txt
@@ -0,0 +1,12 @@
+﻿DSA Artefaktgenerator
+---------------------------
+
+Installation
+------------
+
+Die setup.exe prüft das Vorhandensein von Abhängigkeiten, d.h. die .NET 6.0 Desktop Runtime (x86), und intalliert diese ggf. automatisch um anschließend das Tool selbst (ArtefaktGenerator-Installer.msi) zu installieren.
+
+Quellcode
+---------
+
+ https://github.com/pielmach/dsa-artefaktgenerator
